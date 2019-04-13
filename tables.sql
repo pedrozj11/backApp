81f55a5fafd521d12daf750f4f469e134704bbdf
Pedro <Zapata>
2019-04-13 14:39:28 +0200

    Connection and database scripts.

---
+++ tables.sql
@@ -0,0 +1,22 @@
+
+/* Node_tree Table */ 
+
+CREATE TABLE docebo.node_tree(
+    idNode INT NOT NULL,
+    level INT NOT NULL,
+    iLeft INT NOT NULL,
+    iRight INT NOT NULL,
+    PRIMARY KEY(idNode)
+);
+
+
+
+/* Node_tree_names Table */ 
+
+CREATE TABLE docebo.node_tree_names(
+    idNode INT NOT NULL,
+    language VARCHAR(255),
+    nodeName VARCHAR(255),
+    PRIMARY KEY(idNode),
+    FOREIGN KEY(idNode) REFERENCES docebo.node_tree(idNode)
+);
