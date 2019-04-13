b894b1ba2dca9413021dcfa760d2449ad589a57f
Pedro <Zapata>
2019-04-13 19:06:08 +0200

    Api working. Query in need to be fixed.

--- api.php
+++ api.php
@@ -1,3 +1,28 @@
 <?php
+// Headers
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json');
 
-?>
+include_once 'config.php';
+include_once 'model.php';
+
+
+// Instantiate blog model object
+$model = new Model();
+
+// Blog model query
+$result = $model->read();
+// Get row count
+$num = count($result);
+
+// Check if any results
+if($num > 0) {
+
+  echo json_encode($result);
+
+} else {
+  // No results
+  echo json_encode(
+    array('message' => 'No results')
+  );
+}
---
+++ model.php
@@ -0,0 +1,31 @@
+<?php
+class Model
+{
+    // DB stuff
+    private $table;
+    private $db;
+    private $conectar;
+
+    // Constructor with DB
+    public function __construct()
+    {
+        $this->table = 'node_tree_names';
+        require_once 'config.php';
+        $this->conectar = new Config();
+        $this->db = $this->conectar->connection();
+    }
+
+    // Get Nodes
+    public function read()
+    {
+
+        // Create query
+        $query = $this->db->query("SELECT * FROM $this->table");
+        print_r($query);
+        while ($row = $query->fetch_object()) {
+            $resultSet[] = $row;
+        }
+
+        return $resultSet;
+    }
+}
